6ba25a1c1b16d0c99057c40ff5411a5d9504dee7
滕国栋 <juryfuture@gmail.com>
2017-05-19 18:33:25 +0800

    sorter

---
+++ sorter/src/algorithms/qsort/qsort.go
@@ -0,0 +1,37 @@
+package qsort
+
+func quickSort(values []int, left, right int) {
+	temp := values[left]
+	p := left
+	i, j := left,right
+
+	for i <= j {
+		for j >= p && values[j] >= temp {
+			j--
+		}
+		if j >= p {
+			values[p] = values[j]
+			p = j
+		}
+
+		for i <= p && values[i] <= temp {
+			i++
+		}
+		if i <= p {
+			values[p] = values[i]
+			p = i
+		}
+	}
+	values[p] = temp
+
+	if p > left {
+		quickSort(values, left, p-1)
+	}
+	if p < right {
+		quickSort(values, p+1, right)
+	}
+}
+
+func QuickSort(values []int) {
+	quickSort(values, 0, len(values)-1)
+}
