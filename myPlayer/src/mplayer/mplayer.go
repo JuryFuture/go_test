92721bd1b1042404b050431cbc9782d8268177f2
JuryFuture <juryfuture@gmail.com>
2017-05-27 17:45:18 +0800

    音乐播放器

--- myPlayer/src/mplayer/mplayer.go
+++ myPlayer/src/mplayer/mplayer.go
@@ -11,46 +11,47 @@ import(
 	"mp"
 )
 
-var lib *library.MusicManager
+var lib = new(library.MusicManager)
 var id int = 1
 var ctrl, signal chan int
 
 func handleLibCommands(tokens []string) {
-	switch tockens[1] {
+	switch tokens[1] {
 		case "list":
 			for i := 0; i < lib.Len(); i++ {
 				m,_ := lib.Get(i)
 				fmt.Println(i+1,":", m.Name, m.Artist, m.Source, m.Type)
 			}
 		case "add":
-			if len(tockens) == 6 {
+			if len(tokens) == 6 {
 				id++
-				lib.Add(&MusicEntry{id,tockens[2],tockens[3],tockens[4],tockens[5]})
+				lib.Add(&library.MusicEntry{strconv.Itoa(id),tokens[2],tokens[3],tokens[4],tokens[5]})
 			} else {
 				fmt.Println("USAGE: lib add <name><srtist><source><type>")
 			}
 		case "remove":
-			if len(tockens) ==3 {
-				lib.Remove(strconv.Atoi(tockens[2]))
+			if len(tokens) ==3 {
+				index,_ := strconv.Atoi(tokens[2])
+				lib.Remove(index)
 			} else {
 				fmt.Println("USAGE: lib remove <index>")
 			}
 		default:
-			fmt.Println("Unrecognized lib command:",tockens[0])
+			fmt.Println("Unrecognized lib command:",tokens[0])
 	}
 }
 
-func handlePlayCommand(tockens []string) {
-	if len(tockens) != 2 {
+func handlePlayCommands(tokens []string) {
+	if len(tokens) != 2 {
 		fmt.Println("USAGE: play <name>")
 		return
 	}
 
-	e := lib.Find(tockens[1])
+	e := lib.Find(tokens[1])
 
 
 	if e ==nil {
-		fmt.Println("The music", tockens[1], "not exist.")
+		fmt.Println("The music", tokens[1], "not exist.")
 	}
 
 	mp.Play(e.Source, e.Type)
@@ -59,7 +60,6 @@ func handlePlayCommand(tockens []string) {
 func main() {
 	fmt.Println("Enter following commands to control the player:\nlib list -- View the existing music lib\nlib add <name><artist><source><type> -- Add a music to the music lib\nlib remove <name> -- Remove the specified music from the lib\nplay <name> -- Play the specified music")
 
-	lib := library.NewMusicManager()
 	r := bufio.NewReader(os.Stdin)
 
 	for {
@@ -72,14 +72,14 @@ func main() {
 			break
 		}
 
-		tockens := strings.Split(line, " ")
+		tokens := strings.Split(line, " ")
 
-		if tockens[0] == "lib" {
-			handleLibCommand(tockens)
-		} else if tockens[0] == "play" {
-			handlePlayCommand(tockens)
+		if tokens[0] == "lib" {
+			handleLibCommands(tokens)
+		} else if tokens[0] == "play" {
+			handlePlayCommands(tokens)
 		} else {
-			fmt.Println("Unrecognized command:", tockens[0])
+			fmt.Println("Unrecognized command:", tokens[0])
 		}
 	}
 }
